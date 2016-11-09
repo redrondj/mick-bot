9a0216b603c4674843b8486fd2af41433ee95c27
redrondj <redrondj@gmail.com>
2016-11-09 14:09:35 -0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+  recast: {
+    token: '205d3c7852a70a8b082c71157958e31b',
+    language: 'pt-br',
+  },
+  slack: {
+    token: 'xoxb-102556996517-WGt96SIxfMeNQyVUbVbPqU6g',
+  },
+  port: 8080,
+}
+
+module.exports = config
